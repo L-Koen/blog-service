439a47eeb66fc64a1ffcafc86fe9ca397ae12501
L-Koen <lauw.koen@gmail.com>
2025-02-11 19:05:25 +0100

    Add .env

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+# Load environment variables
+ENV_FILE = "/app/.env"
+load_dotenv(ENV_FILE)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=@509t(zf5n4o*-9&u$22k2@*rgyw=2v=)=m4livcm+9*6d0x*"
+SECRET_KEY = os.getenv("SECRET_KEY") # Get it from .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv(DEBUG, "False")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
@@ -37,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "posts"
 ]
 
 MIDDLEWARE = [
